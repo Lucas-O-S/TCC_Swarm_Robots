a1a9628807f4d867dfa3178605dc07e52738f61f
LucasOSilvaTaak <lucas.silva@taak.com.br>
2026-05-20 12:19:11 -0300

    arrumando o swagger

--- Backend/server/src/main.ts
+++ Backend/server/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import 'dotenv/config';
 import { AppModule } from './app.module';
+import { DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
+  
+  const config = new DocumentBuilder()
+  .setTitle('TCC Swarm Robots')
+  .setDescription('API for the TCC Swarm Robots project')
+  .setVersion('1.0')
+  .addBearerAuth()
+  .build();
+
   await app.listen(process.env.PORT ?? 3000);
+
 }
 bootstrap();
