1367c23025c0e3727523a0dbdf20019047de6fbd
Lucas-O-S <lucasoscomercial@gmail.com>
2026-07-09 11:32:57 -0300

    Update docker-compose.yml to include volume mappings for PostgreSQL and add RobotModule to app.module.ts for improved database initialization and module organization.

--- Backend/server/src/app.module.ts
+++ Backend/server/src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { sequelizeConfig } from './config/sequelize.config';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { RobotModule } from './Classes/Robots/Robot.module';
 
 @Module({
   imports: [
-    SequelizeModule.forRoot(sequelizeConfig)
+    SequelizeModule.forRoot(sequelizeConfig),
+    RobotModule,
   ],
   controllers: [AppController],
   providers: [AppService],
