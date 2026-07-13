fd2f94fb50626c54d2b2321a55cf44777166601a
LucasOSilvaTaak <lucas.silva@taak.com.br>
2026-07-13 16:57:02 -0300

    Add MovePayload interface and codec for encoding movement data

---
+++ Backend/server/src/Protocol/Payloads/MovePayload.COdec.ts
@@ -0,0 +1,25 @@
+import { PayloadCodec } from "./Protocol.Codec";
+
+
+export interface MovePayload {
+    left_x: number,
+    left_y: number,
+    right_x: number,
+    right_y: number
+}
+
+
+export class MovePayload implements PayloadCodec<MovePayload> {
+
+    encodePayload (payload: MovePayload): Buffer {
+
+        const buffer = Buffer.alloc(4)
+
+        buffer.writeUInt8(payload.left_x, 0);
+        buffer.writeUInt8(payload.left_y, 1);
+        buffer.writeUInt8(payload.right_x, 2);
+        buffer.writeUInt8(payload.right_y, 3);
+
+        return buffer;
+    }
+}
